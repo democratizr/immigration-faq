67ecbb168d5a1569543d810f15a343ec32e4b8d5
eyelidlessness <gnosis@gmail.com>
2017-05-28 10:52:33 -0700

    React.PropTypes -> PropTypes
    This doesn't resolve the warnings in this codebase, because (presumably) underlying libs are also referring to React.PropTypes

--- src/layouts/Post/index.js
+++ src/layouts/Post/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from "react"
+import PropTypes from 'prop-types';
+import React from 'react';
 
 import LatestPosts from "../../components/LatestPosts"
 import Page from "../Page"
